aeff56ae219fbf8b2ac21176b6e096351a47943a
Ron Cley <124509262+reign33@users.noreply.github.com>
2023-12-19 21:17:53 +0800

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.css";
+
+function App() {
+  return (
+    <div className="text-[30px] text-[Yellow] bg-[green] text-center font-bold">App</div>
+  )
+}
+
+export default App
