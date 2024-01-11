87801b7784b7cb7149891cbcf54378c14cd845b3
Ron Cley <124509262+reign33@users.noreply.github.com>
2024-01-12 03:39:05 +0800

    ongoing add unit

---
+++ Backend/model/Unit.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const unitSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+unitSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Units = mongoose.model("Units", unitSchema);
+
+export default Units;
+
+//npm i bcrypt to transform passwordhash to encrypted type
